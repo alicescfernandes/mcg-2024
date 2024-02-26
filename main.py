ac53a18e92dbcd2dc340fac5678801908b82052b
Alice Fernandes <alicescfernandes@gmail.com>
2024-02-26 19:14:10 +0000

    moar stuff

--- main.py
+++ main.py
@@ -10,5 +10,19 @@ class Quadrado:
     return f"lado: {self.lado}"
 
 
-instancia = Quadrado(3)
-print("instancia", instancia)
+q1 = Quadrado(3)
+print(q1, q1.area())
+
+
+class Circulo:
+  def __init__(self, raio):
+    self.raio = raio
+
+  def area(self):
+    return 3.14 * self.raio ** 2
+    
+  def __repr__(self):
+    return f"raio: {self.raio}"
+
+circulo = Circulo(2)
+print(circulo, circulo.area())
